d3a17c46ad74026f97c7e0142ea1e855565d0672
chikocharles <charliechicco@gmail.com>
2025-06-23 16:56:21 +0200

    logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -48,7 +48,7 @@ const Footer = () => {
           >
             <div className="flex items-center space-x-2 mb-6">
                <img  
-                src="https://photos.google.com/album/AF1QipPCeKKZ3dr8zVHzGbVrP9ObiQ7xSqoot-dGeYVt/photo/AF1QipNmv_HEtdN7IJhj8MAx5EmcvzSTALkCysOYtN-s" 
+                src="logo2.png" 
                 alt="Geosync Detectors Logo" 
                 className="h-10 w-auto"
                 />
